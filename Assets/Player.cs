9d9f2457badfac91e39a47a10114ab8345e3a3c1
金伟成 <>
2019-01-30 11:27:42 +0800

    Init Commit

---
+++ Assets/Player.cs
@@ -0,0 +1,67 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;//84 使用unity自带的预设//
+
+public class Player : MonoBehaviour {
+
+    [Tooltip("In ms^-1")][SerializeField] float Speed = 10f;
+    [Tooltip("In m")] [SerializeField] float xRange = 5f;
+    [Tooltip("In m")] [SerializeField] float yRange = 3f;
+
+    [SerializeField] float positionPitchFactor = -5f;
+    [SerializeField] float controlPitchFactor = -20f;
+    [SerializeField] float positionYawFactor = 5f;
+    [SerializeField] float controlRollFactor = -20f;
+
+
+    float xThrow, yThrow;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ProcessTranslation();
+        ProcessRotation();
+
+    }
+
+    private void ProcessRotation(){
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;  //88 随着y变化旋转
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+
+        float yaw = transform.localPosition.x * positionYawFactor;
+
+        float roll = xThrow * controlRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+        //local是指针对子集-飞机 的旋转。Euler而不是rotation.y的原因是这是符合herichy的定义的xyz
+
+
+    }
+
+
+    private void ProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        //84 Thorw是指-1到1的偏移//
+        float xOffset = xThrow * Speed * Time.deltaTime;
+        float yOffset = yThrow * Speed * Time.deltaTime;
+        //85 乘这帧花的时间，来 保持总匀速//
+
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);//限制范围//
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+        //86 解决加速与出边界
+    }
+}
